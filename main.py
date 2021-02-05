64d3074f5e2edd17ee4da9669efe08511b32158e
clvrk <52180876+clvrk@users.noreply.github.com>
2021-02-05 10:27:24 -0800

    Changes to processes

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-from PIL import Image
-import requests
 import discord
 from discord.ext import commands
 from io import BytesIO
 import os
+import aiohttp
 from dotenv import load_dotenv
 client = commands.Bot(command_prefix='!', help_command=None)
 
 load_dotenv()
 
 @client.command()
-async def createemoji(ctx, object, *, name):
+async def createemoji(ctx, url: str, *, name):
 	guild = ctx.guild
 	if ctx.author.guild_permissions.manage_emojis:
-		r = requests.get(object)
-		img = Image.open(BytesIO(r.content), mode='r')
-		try:
-			img.seek(1)
-
-		except EOFError:
-			is_animated = False
-
-		else:
-			is_animated = True
-
-		if is_animated == True:
-			await ctx.send("We do not support animated emojis!")
-
-		elif is_animated == False:
-			b = BytesIO()
-			img.save(b, format='PNG')
-			b_value = b.getvalue()
-			emoji = await guild.create_custom_emoji(image=b_value, name=name)
-			await ctx.send(f'Successfully created emoji: <:{name}:{emoji.id}>')
+		async with aiohttp.ClientSession() as ses:
+			async with ses.get(url) as r:
+				
+				try:
+					img_or_gif = BytesIO(await r.read())
+					b_value = img_or_gif.getvalue()
+					if r.status in range(200, 299):
+						emoji = await guild.create_custom_emoji(image=b_value, name=name)
+						await ctx.send(f'Successfully created emoji: <:{name}:{emoji.id}>')
+					else:
+						await ctx.send(f'Error when making request | {r.status} response.')
+						
+				except discord.HTTPException:
+					await ctx.send('File size is too big!')
 
 
 
